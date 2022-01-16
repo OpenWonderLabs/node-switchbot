0ea9daf87c002e92b78d092dfe4b755d9493b65e
Donavan Becker <beckersmarthome@icloud.com>
2022-01-16 12:01:29 -0600

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
