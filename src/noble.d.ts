ca7c3dde4f2fe3e4d7ae16325ff8c5fbe606dc74
Dave Nicolson <david.nicolson@gmail.com>
2024-05-06 19:17:47 +0200

    Update Noble

--- src/noble.d.ts
+++
@@ -1,7 +0,0 @@
-// This can be removed after https://github.com/abandonware/noble/pull/333 is merged
-
-export * from '@abandonware/noble';
-
-declare module '@abandonware/noble' {
-  const _state: 'unknown' | 'resetting' | 'unsupported' | 'unauthorized' | 'poweredOff' | 'poweredOn';
-}
