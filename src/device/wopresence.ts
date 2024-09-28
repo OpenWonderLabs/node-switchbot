f88f13eda05c1891eb4afc69025da13a9ea54be2
Donavan Becker <beckersmarthome@icloud.com>
2024-09-28 17:20:04 -0500

    Update wopresence.ts

--- src/device/wopresence.ts
+++ src/device/wopresence.ts
@@ -1,51 +1,47 @@
-import type { Buffer } from 'node:buffer'
-
-import type { motionSensorServiceData } from '../types/bledevicestatus.js'
-
 /* Copyright(C) 2024, donavanbecker (https://github.com/donavanbecker). All rights reserved.
  *
  * wopresence.ts: Switchbot BLE API registration.
  */
+import type { Buffer } from 'node:buffer'
+
+import type { motionSensorServiceData } from '../types/bledevicestatus.js'
+
 import { SwitchbotDevice } from '../device.js'
 import { SwitchBotBLEModel, SwitchBotBLEModelFriendlyName, SwitchBotBLEModelName } from '../types/types.js'
 
+/**
+ * Class representing a WoPresence device.
+ */
 export class WoPresence extends SwitchbotDevice {
+  /**
+   * Parses the service data for WoPresence.
+   * @param {Buffer} serviceData - The service data buffer.
+   * @param {Function} [onlog] - Optional logging function.
+   * @returns {Promise<motionSensorServiceData | null>} - Parsed service data or null if invalid.
+   */
   static async parseServiceData(
     serviceData: Buffer,
-    onlog: ((message: string) => void) | undefined,
+    onlog?: (message: string) => void,
   ): Promise<motionSensorServiceData | null> {
     if (serviceData.length !== 6) {
-      if (onlog && typeof onlog === 'function') {
-        onlog(`[parseServiceDataForWoPresence] Buffer length ${serviceData.length} !== 6!`)
-      }
+      onlog?.(`[parseServiceDataForWoPresence] Buffer length ${serviceData.length} !== 6!`)
       return null
     }
 
-    const byte1 = serviceData.readUInt8(1)
-    const byte2 = serviceData.readUInt8(2)
-    const byte5 = serviceData.readUInt8(5)
-
-    const tested = !!(byte1 & 0b10000000)
-    const movement = !!(byte1 & 0b01000000)
-    const battery = byte2 & 0b01111111
-    const led = (byte5 & 0b00100000) >> 5
-    const iot = (byte5 & 0b00010000) >> 4
-    const sense_distance = (byte5 & 0b00001100) >> 2
-    const lightLevel = byte5 & 0b00000011
-    const is_light = !!(byte5 & 0b00000010)
+    const [byte1, byte2, , , , byte5] = serviceData
 
     const data: motionSensorServiceData = {
       model: SwitchBotBLEModel.MotionSensor,
       modelName: SwitchBotBLEModelName.MotionSensor,
       modelFriendlyName: SwitchBotBLEModelFriendlyName.MotionSensor,
-      tested,
-      movement,
-      battery,
-      led,
-      iot,
-      sense_distance,
-      lightLevel: lightLevel === 1 ? 'dark' : lightLevel === 2 ? 'bright' : 'unknown',
-      is_light,
+      tested: !!(byte1 & 0b10000000),
+      movement: !!(byte1 & 0b01000000),
+      battery: byte2 & 0b01111111,
+      led: (byte5 & 0b00100000) >> 5,
+      iot: (byte5 & 0b00010000) >> 4,
+      sense_distance: (byte5 & 0b00001100) >> 2,
+      lightLevel: (byte5 & 0b00000011) === 1 ? 'dark' : (byte5 & 0b00000011) === 2 ? 'bright' : 'unknown',
+      is_light: !!(byte5 & 0b00000010),
     }
 
     return data
